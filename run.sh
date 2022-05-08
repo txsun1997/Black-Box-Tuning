d0588158001bc684509c0e077f9ed13e56c5b528
Hzfinfdu <zfhe19@fudan.edu.cn>
2022-05-08 02:41:37 +0000

    reset run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,12 @@
-for task in "mrpc" "rte" "snli" "agnews" "sst2" "dbpedia" "yelpp"
-do
-CUDA_VISIBLE_DEVICES=0 python deepbbt.py \
-  --task_name $task \
+python bbt.py \
+  --task_name "sst2" \
   --n_prompt_tokens 50 \
   --intrinsic_dim 500 \
   --k_shot 16 \
   --device "cuda:0" \
-  --seed 100 \
+  --seed 42 \
   --loss_type "hinge" \
   --cat_or_add "add" \
-  --budget 8000 \
+  --budget 5000 \
   --print_every 50 \
-  --eval_every 100 \
-  --inference_framework 'ort' \
-  --onnx_model_path './onnx_models/deep_optimized_model.onnx'
-done
+  --eval_every 100
